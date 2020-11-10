ed9485f38ccecd977b80cfe9f8222dad31652d3c
Vitaly Tsaplin <vitaly@tsaplin.com>
2020-11-10 09:46:45 +0100

    Avoid parsing proxy configuration globaly (#1)
    Co-authored-by: Vitaly Tsaplin <tsaplin@adobe.com>

--- bin/index.js
+++ bin/index.js
@@ -9,15 +9,14 @@ accordance with the terms of the Adobe license agreement accompanying
 it.
 */
 
-const path = require('path');
 const shell = require('shelljs');
 const proxy = require('../lib/proxy');
-const proxyConfig = require('../lib/proxyConfig');
 
 require('yargs')
   .scriptName('aem-site-theme-builder')
   .usage('$0 <cmd> [args]')
   .command('live', 'Runs live preview of your theme with content from AEM', (yargs) => {}, function (argv) {
+    const proxyConfig = require('../lib/proxyConfig');
     proxy(proxyConfig);
   })
   .command('deploy', 'Deploys your theme to AEM instance', (yargs) => {}, function (argv) {
