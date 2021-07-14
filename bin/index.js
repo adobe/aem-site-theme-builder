815f2a51b851896e522831c1db59093c19d93696
Bartosz Głowacki <bartosz.glow@gmail.com>
2021-07-14 13:18:07 +0200

    Browser sync support with dynamic ports (#11)
    * findPort function for proxy to get a free port
    
    * BrowserSync part of the live command
    
    * readme update with proxy / live API update
    
    * code review improvement

--- bin/index.js
+++ bin/index.js
@@ -11,13 +11,24 @@ it.
 
 const shell = require('shelljs');
 const proxy = require('../lib/proxy');
+const browserSync = require('../lib/browserSync');
+const findPort = require('../lib/findPort');
 
 require('yargs')
   .scriptName('aem-site-theme-builder')
   .usage('$0 <cmd> [args]')
-  .command('live', 'Runs live preview of your theme with content from AEM', (yargs) => {}, function (argv) {
-    const proxyConfig = require('../lib/proxyConfig');
-    proxy(proxyConfig);
+  .command('proxy', 'Runs proxy to preview your theme with content from AEM', (yargs) => {}, async function (argv) {
+    const CONFIG = require('../lib/proxyConfig');
+    const port = await findPort(parseInt(CONFIG.port, 10));
+    proxy({ ...CONFIG, port });
+  })
+  .command('live', 'Runs live preview (proxy + browser sync) of your theme with content from AEM', (yargs) => {}, async function (argv) {
+    const CONFIG = require('../lib/proxyConfig');
+    const portProxy = await findPort(parseInt(CONFIG.port, 10));
+    await proxy({ ...CONFIG, port: portProxy });
+    // wait for proxy to start before finding next port for browser sync
+    const portBrowserSync = await findPort(portProxy);
+    browserSync({ port: portBrowserSync, portProxy });
   })
   .command('deploy', 'Deploys your theme to AEM instance', (yargs) => {}, function (argv) {
     shell.exec('dotenv -- cross-var curl -d \'site=%AEM_SITE%\' -d \'artifact=%GIT_ARTIFACT_ID%\' -d \'expiration=%GIT_HASH_EXPIRATION%\' -d \'hash=%GIT_HASH%\' %AEM_URL%/aem/update.theme.html');
